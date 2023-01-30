72c1e57fda7ec1348e4e6ef8c12068ab574f09c5
wimadriaensen <r0445498@student.thomasmore.be>
2023-01-30 15:46:24 +0100

    final version

--- project_test.py
+++ project_test.py
@@ -25,7 +25,7 @@ def running(list, speed):
 
 @app.route("/")
 def hello():
-    return render_template('index.html')
+    return render_template('index.html', data=[{'difficulty':'easy'}, {'difficulty':'medium'}, {'difficulty':'hard'}] )
 
 
 @app.route("/start", methods=['GET', 'POST'])
@@ -36,6 +36,8 @@ def start():
         name1 = form_data['speler1']
         global name2
         name2 = form_data['speler2']
+        global difficulty
+        difficulty = form_data['diff_select']
 
         session['result'] = [name1, name2]
 
@@ -65,23 +67,55 @@ def schot1():
         #----------------------
         #------ Rekensom ------
 
-        # global getal1
-        getal1 = random.randint(50, 110)
-        # global getal2 
-        getal2 = random.randint(50, 110)
+        if difficulty == "easy":
+            getal1 = random.randint(10, 50)
+            getal2 = random.randint(10, 50)
+            getal3 = random.randint(1, 10)
+            getal4 = random.randint(1, 10)
+            if getal2 > getal1:
+                grootste_getal = getal2
+                getal2 = getal1
+                getal1 = grootste_getal
+        elif difficulty == "medium":
+            getal1 = random.randint(50, 100)
+            getal2 = random.randint(50, 100)
+            getal3 = random.randint(10, 20)
+            getal4 = random.randint(10, 20)
+        else:
+            getal1 = random.randint(70, 110)
+            getal2 = random.randint(70, 110)
+            getal3 = random.randint(5, 20)
+
 
         global uitkomst1 
-
+        
         random_som = random.randint(1,3)
+
         if random_som == 1:
-            som = str(getal1)+" + " + str(getal2)
-            uitkomst1 = getal1+getal2
+            if difficulty == "hard":
+                som = str(getal1)+" + " + str(getal2) + " x " + str(getal3)
+                uitkomst1 = getal1+getal2*getal3
+
+            else:
+                som = str(getal1)+" + " + str(getal2)
+                uitkomst1 = getal1+getal2
         elif random_som == 2:
-            som = str(getal1)+" x " + str(getal2)
-            uitkomst1 = getal1*getal2
+            if difficulty == "hard":
+                som = str(getal3)+" x " + str(getal2) + " - " + str(getal1)
+                uitkomst1 = getal3*getal2-getal1
+                
+            else:
+                som = str(getal1)+" - " + str(getal2)
+                uitkomst1 = getal1-getal2
         else:
-            som = str(getal1)+" - " + str(getal2)
-            uitkomst1 = getal1-getal2
+            if difficulty == "hard":
+                som = str(getal1)+" - " + str(getal2) + " x " + str(getal3)
+                tussen_uitkomst = getal1-getal2*getal3
+                uitkomst1 = round(tussen_uitkomst, 2)
+
+            else:
+                som = str(getal3)+" x " + str(getal4)
+                uitkomst1 = getal3*getal4
         
         #----------------------
 
@@ -127,43 +161,60 @@ def schot2():
             counter2 += 1
 
         #------ Rekensom ------------
-        # nog te doen
-        # global getal1
-        getal1 = random.randint(50, 110)
-        # global getal2 
-        getal2 = random.randint(50, 110)
-
-        global uitkomst2 
-
-        random_som = random.randint(1,3)
-        if random_som == 1:
-            som = str(getal1)+" + " + str(getal2)
-            uitkomst2 = getal1+getal2
-        elif random_som == 2:
-            som = str(getal1)+" x " + str(getal2)
-            uitkomst2 = getal1*getal2
+        if difficulty == "easy":
+            getal1 = random.randint(10, 50)
+            getal2 = random.randint(10, 50)
+            getal3 = random.randint(1, 10)
+            getal4 = random.randint(1, 10)
+            if getal2 > getal1:
+                grootste_getal = getal2
+                getal2 = getal1
+                getal1 = grootste_getal
+        elif difficulty == "medium":
+            getal1 = random.randint(50, 100)
+            getal2 = random.randint(50, 100)
+            getal3 = random.randint(10, 20)
+            getal4 = random.randint(10, 20)
         else:
-            som = str(getal1)+" - " + str(getal2)
-            uitkomst2 = getal1-getal2
-        #---------------------------
+            getal1 = random.randint(70, 110)
+            getal2 = random.randint(70, 110)
+            getal3 = random.randint(5, 20)
+
+
+        global uitkomst2
+        
+        random_som = random.randint(1,3)
+
+        if random_som == 1:
+            if difficulty == "hard":
+                som = str(getal1)+" + " + str(getal2) + " x " + str(getal3)
+                uitkomst2 = getal1+getal2*getal3
+
+            else:
+                som = str(getal1)+" + " + str(getal2)
+                uitkomst2 = getal1+getal2
+        elif random_som == 2:
+            if difficulty == "hard":
+                som = str(getal3)+" x " + str(getal2) + " - " + str(getal1)
+                uitkomst2 = getal3*getal2-getal1
+                
+            else:
+                som = str(getal1)+" - " + str(getal2)
+                uitkomst2 = getal1-getal2
+        else:
+            if difficulty == "hard":
+                som = str(getal1)+" - " + str(getal2) + " x " + str(getal3)
+                tussen_uitkomst = getal1-getal2*getal3
+                uitkomst2 = round(tussen_uitkomst, 2)
+
+            else:
+                som = str(getal3)+" x " + str(getal4)
+                uitkomst2 = getal3*getal4
+                
         
         
         return render_template('schot2.html', result=[power, som] )
 
-# @app.route("/end", methods=['GET', 'POST'])
-# def end():
-#     if request.method == 'POST':
-#         form_data = request.form
-    
-#         # global score_p2
-#         # print(form_data['score2'])
-#         score_p2 = int(form_data['score2'])
-#         som_p2 = int(form_data['som2'])
-
-#         if som_p2 == uitkomst2:
-#             score_p2 += 3
-
-#     return render_template('end.html', result=[name1, name2, score_p1, score_p2])
 
 @app.route("/end", methods=['GET', 'POST'])
 def end():
